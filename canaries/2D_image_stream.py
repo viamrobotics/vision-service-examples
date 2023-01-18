aab21156c539cc339910112f6696f3cc322df1b8
Clyde Bazile <clyde@viam.com>
2023-01-18 13:47:32 -0500

    revert: redirect python output to stdout

--- canaries/2D_image_stream.py
+++ canaries/2D_image_stream.py
@@ -18,19 +18,19 @@ async def connect():
     logging.info("connecting to robot client")
     creds = Credentials(
         type='robot-location-secret',
-        payload='69h68573npk80j95il8tvbl7t1zqyzleukdipvd7y58te1k5')
+        payload='gv3b40fw2ii1a0g5dfhfu7i085f7rfj8jm2oh692znd0sedj')
     opts = RobotClient.Options(
         refresh_interval=0,
         dial_options=DialOptions(credentials=creds)
     )
-    return await RobotClient.at_address('holy-sun-main.h78a7zlqox.viam.cloud', opts)
+    return await RobotClient.at_address('2d-stream-main.i2z4laurah.viam.cloud', opts)
 
 
 async def main():
     robot = await connect()
     logging.info("finished connecting to robot client")
 
-    cam_name = "cam"
+    cam_name = "standard_camera"
     cam = Camera.from_robot(robot, cam_name)
     logging.info(f"found camera {cam_name}")
 
@@ -46,10 +46,21 @@ async def main():
 
         logging.info("displaying window")
         while True:
-            await cam.get_image(CameraMimeType.JPEG)
+            pil_img = await cam.get_image()
+            logging.info("image received")
             llist.append(datetime.datetime.now())
+
+            draw = ImageDraw.Draw(pil_img)
+            draw.rectangle((0, 0, 75, 25), outline='black')
             fps = get_frames_per_sec()
-            logging.info(f"fps={fps}")
+            draw.text(xy=(18, 7.5), text=f'{fps} FPS', fill='white')
+
+            open_cv_image = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+            window_name = '2D'
+            cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty(window_name,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+            cv2.imshow(window_name, open_cv_image)
+            cv2.waitKey(1)
     finally:
         logging.info("closing robot")
         await robot.close()
