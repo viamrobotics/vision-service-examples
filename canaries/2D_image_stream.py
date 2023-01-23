d7d9793c54c2d639229243544ebae29fce3747c6
Clyde Bazile <clyde@viam.com>
2023-01-23 13:47:31 -0500

    restart viam-server on failures

--- canaries/2D_image_stream.py
+++ canaries/2D_image_stream.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+import time
 
 import cv2
 import datetime
@@ -39,6 +41,11 @@ def get_frames_per_sec(ordered_frames):
         ordered_frames.popleft()
     return len(ordered_frames)
 
+def restart_server():
+    logging.info("restarting the viam-server...")
+    os.system("systemctl restart viam-server")
+    time.sleep(30)  # give the viam-server time to restart
+    logging.info("viam-server up")
 
 async def main():
     robot = None
@@ -78,8 +85,9 @@ async def main():
         except Exception as e:
             logging.info(f"caught exception '{e}'")
             close_robot(robot)
+            restart_server()
             continue
-    logging.info(f"cannot get image. {attempts} tried and failed")
+    logging.info(f"cannot get image. {attempts} attempts tried and failed")
 
 
 if __name__ == '__main__':
