6161ffe4fede41df57ef29a6e9c169b4c6a09953
Martha Johnston <martha.johnston@viam.com>
2023-02-17 21:33:25 +0000

    add object detection canary

--- canaries/2D_image_stream.py
+++ canaries/2D_image_stream.py
@@ -9,12 +9,20 @@ from collections import deque
 from viam.components.camera import Camera
 from viam.robot.client import RobotClient
 from viam.rpc.dial import Credentials, DialOptions
+from viam.components.camera.client import CameraMimeType
 
 import logging
 
+screen_res = 1280, 720
+scale_width = screen_res[0] / 425
+scale_height = screen_res[1] / 480
+scale = min(scale_width, scale_height)
+#resized window width and height
+window_width = int(425 * scale)
+window_height = int(480 * scale)
 
 async def connect():
-    logging.info("connecting to robot client")
+    print("connecting to robot client")
     creds = Credentials(
         type='robot-location-secret',
         payload='gv3b40fw2ii1a0g5dfhfu7i085f7rfj8jm2oh692znd0sedj')
@@ -27,9 +35,9 @@ async def connect():
 
 async def close_robot(robot):
     if robot:
-        logging.info("closing robot")
+        print("closing robot")
         await robot.close()
-        logging.info("robot closed")
+        print("robot closed")
 
 
 def get_frames_per_sec(ordered_frames):
@@ -39,44 +47,98 @@ def get_frames_per_sec(ordered_frames):
         ordered_frames.popleft()
     return len(ordered_frames)
 
+def get_frames_per_sec_detections(ordered_frames):
+    one_sec_ago = datetime.datetime.now() - datetime.timedelta(seconds=1)
+    # evict datetimes greater than one sec ago, i.e., datetimes smaller than now()-one_sec_ago
+    while ordered_frames[0] < one_sec_ago:
+        ordered_frames.popleft()
+    return len(ordered_frames)
+
+async def detection_stream(transform, llist1):
+    while True:
+        # This is to stop this script just before the start of the next hour.
+        current_min = int(datetime.datetime.now().strftime("%M"))
+        if current_min == 59:
+            await close_robot(robot)
+            return
+        detect_img = await transform.get_image(CameraMimeType.JPEG) # default is PNG, JPEG is faster
+        llist1.append(datetime.datetime.now())
+
+        draw = ImageDraw.Draw(detect_img)
+        draw.rectangle((0, 0, 75, 25), outline='black')
+        fps = get_frames_per_sec(llist1)
+        draw.text(xy=(18, 7.5), text=f'{fps} FPS', fill='white')
+
+        pix = cv2.cvtColor(np.array(detect_img, dtype=np.uint8), cv2.COLOR_RGB2BGR)
+        pix = pix[0:480, 0:425]
+
+        # cv2.imwrite('transform.jpg', pix)
+
+        window_name = 'Detections'
+        cv2.namedWindow(window_name, cv2.WND_PROP_VISIBLE)
+        cv2.resizeWindow(window_name, int(window_width), window_height)
+        # cv2.setWindowProperty(window_name, cv2.WND_PROP_VISIBLE, cv2.WINDOW_NORMAL)
+        cv2.imshow(window_name, pix)
+        cv2.waitKey(1)
+    return
+
+async def image_stream(cam, llist2):
+    while True:
+        # This is to stop this script just before the start of the next hour.
+        current_min = int(datetime.datetime.now().strftime("%M"))
+        if current_min == 59:
+            await close_robot(robot)
+            return
+        pil_img = await cam.get_image()
+        llist2.append(datetime.datetime.now())
+
+        draw = ImageDraw.Draw(pil_img)
+        draw.rectangle((0, 0, 75, 25), outline='black')
+        fps = get_frames_per_sec(llist2)
+        draw.text(xy=(18, 7.5), text=f'{fps} FPS', fill='white')
+
+        open_cv_image = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+        open_cv_image = open_cv_image[0:480, 0:425]
+
+        # cv2.imwrite('cam.jpg', open_cv_image)
+
+        window_name = '2D'
+        cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+        cv2.resizeWindow(window_name, window_width, window_height)
+        cv2.moveWindow(window_name, 0, 0)
+	# cv2.setWindowProperty(window_name, cv2.WND_PROP_VISIBLE, cv2.WINDOW_NORMAL)
+        cv2.imshow(window_name, open_cv_image)
+        cv2.waitKey(1)
+    return
 
 async def main():
     robot = None
-    llist = deque()
+    llist1 = deque()
+    llist2 = deque()
     try:
         robot = await connect()
-        logging.info("finished connecting to robot client")
+        print("finished connecting to robot client")
 
         cam_name = "standard_camera"
         cam = Camera.from_robot(robot, cam_name)
-        logging.info(f"found camera {cam_name}")
+        print(f"found camera {cam_name}")
 
-        logging.info("displaying window")
-        while True:
-            # This is to stop this script just before the start of the next hour.
-            current_min = int(datetime.datetime.now().strftime("%M"))
-            if current_min == 59:
-                await close_robot(robot)
-                return
+        transform_cam_name = "transform"
+        transform = Camera.from_robot(robot, transform_cam_name)
+        print(f"found camera {transform_cam_name}")
 
-            pil_img = await cam.get_image()
-            llist.append(datetime.datetime.now())
+        print("displaying window")
 
-            draw = ImageDraw.Draw(pil_img)
-            draw.rectangle((0, 0, 75, 25), outline='black')
-            fps = get_frames_per_sec(llist)
-            draw.text(xy=(18, 7.5), text=f'{fps} FPS', fill='white')
+        task1 = asyncio.create_task(detection_stream(transform, llist1))
+        task2 = asyncio.create_task(image_stream(cam, llist2))
 
-            open_cv_image = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
-            window_name = '2D'
-            cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-            cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-            cv2.imshow(window_name, open_cv_image)
-            cv2.waitKey(1)
+        await task1
+        await task2
+
     except Exception as e:
-        logging.info(f"caught exception '{e}'")
+        print(f"caught exception '{e}'")
         await close_robot(robot)
-        logging.info("exiting with status 1")
+        print("exiting with status 1")
         exit(1)
 
 
@@ -90,8 +152,9 @@ if __name__ == '__main__':
     )
 
     now = lambda: datetime.datetime.now().strftime('%H:%M:%S')
-    logging.info(f"start time {now()}")
+    print(f"start time {now()}")
     try:
         asyncio.run(main())
     finally:
-        logging.info(f"end time {now()}")
+        print(f"end time {now()}")
+
